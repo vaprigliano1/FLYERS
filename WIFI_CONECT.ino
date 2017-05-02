1d35160106b704b663cf1e384053439e079734ec
vaprigliano1 <victor.aprigliano1@hotmail.com>
2017-05-02 15:58:42 -0300

    Update WIFI_CONECT.ino
    Sensor LDR adicionado no código, conexão já funcionando.

--- WIFI_CONECT.ino
+++ WIFI_CONECT.ino
@@ -1,117 +1,88 @@
-
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
-
-const char *ssid = "Iphone de Victor";
-const char *password = "victor1234";
-
-ESP8266WebServer server ( 80 );
-
-const int led = 13;
-
-void handleRoot() {
-	digitalWrite ( led, 1 );
-	char temp[400];
-	int sec = millis() / 1000;
-	int min = sec / 60;
-	int hr = min / 60;
-
-	snprintf ( temp, 400,
-
-"<html>\
-  <head>\
-    <meta http-equiv='refresh' content='5'/>\
-    <title>SALAS DE ESTUDO</title>\
-    <style>\
-      body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
-    </style>\
-  </head>\
-  <body>\
-    <h1>Hello from ESP8266!</h1>\
-    <p>Uptime: %02d:%02d:%02d</p>\
-    <img src=\"/test.svg\" />\
-  </body>\
-</html>",
-
-		hr, min % 60, sec % 60
-	);
-	server.send ( 200, "text/html", temp );
-	digitalWrite ( led, 0 );
+ 
+const char* ssid = ""; //Escrever o nome da rede
+const char* password = ""; //senha
+ 
+int ldr = 0; //definindo LDR na porta A0 da placa
+int valorldr = 0;//armazenando as infos do LDR
+ 
+int id = 1; //Identificação de qual conjunto de sensores está sendo enviado aquela informação
+//(Caso você deseje aplicar mais de um conjunto de sensores)
+ 
+String myIPAddrStr; //Variável para armazenamento do IP gerado para acesso do Webservice.
+// Se quiser criar um IP fixo (vamos tentar),só precisa descomentar as linhas 14,15,16.
+//IPAddress ip(192,168,72,171); //Alterar aqui para o IP Desejado
+//IPAddress gateway(192,168,72,1); //Gateway da rede
+//IPAddress subnet(255,255,255,0);//Mascara da rede
+ 
+WiFiServer server(80); //Definição para o webservice utilizar a porta 80.
+ 
+void setup() {
+Serial.begin(9600);
+delay(10);
+ 
+// inicia a conexão com a rede
+Serial.println();
+Serial.println();
+Serial.print("ESP8266 Conectando à ");
+Serial.println(ssid);
+ 
+WiFi.begin(ssid, password); //Inicia a conexão WiFi
+//WiFi.config(ip, gateway, subnet); //se usar IP fixo, descomentar essa linha
+while (WiFi.status() != WL_CONNECTED) {
+delay(500);
+Serial.print(".");
 }
-
-void handleNotFound() {
-	digitalWrite ( led, 1 );
-	String message = "File Not Found\n\n";
-	message += "URI: ";
-	message += server.uri();
-	message += "\nMethod: ";
-	message += ( server.method() == HTTP_GET ) ? "GET" : "POST";
-	message += "\nArguments: ";
-	message += server.args();
-	message += "\n";
-
-	for ( uint8_t i = 0; i < server.args(); i++ ) {
-		message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
-	}
-
-	server.send ( 404, "text/plain", message );
-	digitalWrite ( led, 0 );
+Serial.println("Conectado");
+server.begin();
+myIPAddrStr = String(WiFi.localIP().toString());
+Serial.println(WiFi.localIP()); //utilizar o IP gerado aqui.
 }
-
-void setup ( void ) {
-	pinMode ( led, OUTPUT );
-	digitalWrite ( led, 0 );
-	Serial.begin ( 115200 );
-	WiFi.begin ( ssid, password );
-	Serial.println ( "" );
-
-	// Wait for connection
-	while ( WiFi.status() != WL_CONNECTED ) {
-		delay ( 500 );
-		Serial.print ( "." );
-	}
-
-	Serial.println ( "" );
-	Serial.print ( "Connected to " );
-	Serial.println ( ssid );
-	Serial.print ( "IP address: " );
-	Serial.println ( WiFi.localIP() );
-
-	if ( MDNS.begin ( "esp8266" ) ) {
-		Serial.println ( "MDNS responder started" );
-	}
-
-	server.on ( "/", handleRoot );
-	server.on ( "/test.svg", drawGraph );
-	server.on ( "/inline", []() {
-		server.send ( 200, "text/plain", "this works as well" );
-	} );
-	server.onNotFound ( handleNotFound );
-	server.begin();
-	Serial.println ( "HTTP server started" );
+ 
+void loop() {
+delay(2000);
+valorldr = analogRead(ldr);
+ 
+WiFiClient client = server.available(); // Comparação se o cliente Wifi, está disponível
+if (!client) {
+return;
 }
-
-void loop ( void ) {                           //ADICIONAR O CODIGO
-	server.handleClient();
-  
+ 
+Serial.println("Webserver gerado.");
+while(!client.available()){
+delay(1);
 }
-
-void drawGraph() {
-	String out = "";
-	char temp[100];
-	out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
- 	out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
- 	out += "<g stroke=\"black\">\n";
- 	int y = rand() % 130;
- 	for (int x = 10; x < 390; x+= 10) {
- 		int y2 = rand() % 130;
- 		sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
- 		out += temp;
- 		y = y2;
- 	}
-	out += "</g>\n</svg>\n";
-
-	server.send ( 200, "image/svg+xml", out);
+String req = client.readStringUntil('\r');
+Serial.println(req);
+client.flush();
+String url = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n";
+//Geração da página HTML
+url += "<html>\r\n";
+url += "<title>Monitoramento de Salas de Estudo</title>";
+url += "<center><h1>Acompanhe as salas de estudo:</h1>";
+url += "<br/>";
+url += "<br/>";
+url += "<br/>";
+url += "<b>Sala 1 = </b>"; //adicionar : if luminosidade > x (print:indisponivel)
+url += valorldr;
+url += "<br/>";
+url += "<b>Intens. Sinal = </b> "; 
+url += "<br/>";
+url += "<b>IP = </b>";
+url += String(myIPAddrStr);
+url += "<br/>";
+url += "<input type=""button"" value=""Atualizar"" onClick=""history.go(0)"">" ; //Essa linha gera um botão p/ atualizar a página do webservice.
+url += "<br/>";
+url += "<br/>";
+url += "<br/>";
+url += "<br/>";
+url += "<br/>";
+url += "<br/>";
+url += "<b>Obrigado por usar a V5. Bons Estudos! </b> ";
+url += "</center></html>\n";
+client.print(url);
+ 
+Serial.println();
+Serial.println("Fechando a conexão");
+delay(30000); //Espera 30 segundos e recomeça o loop.
 }
